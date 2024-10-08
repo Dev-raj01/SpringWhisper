74586f2e75be342151c3a50b836e7bf339e37e04
Dev-raj01 <imdevilrj@gmail.com>
2024-10-09 00:16:59 +0530

    initial repo from starter spring boot project
    Signed-off-by: Dev-raj01 <imdevilrj@gmail.com>

---
+++ src/main/java/com/lamma/SpringWhisper/SpringWhisperApplication.java
@@ -0,0 +1,13 @@
+package com.lamma.SpringWhisper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWhisperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWhisperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamma/SpringWhisper/SpringWhisperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamma.SpringWhisper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWhisperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
