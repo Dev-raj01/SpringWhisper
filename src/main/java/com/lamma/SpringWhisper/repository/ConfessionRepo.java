03466f5c748694ec9160d5572ac4d343bc0691d3
Dev-raj01 <imdevilrj@gmail.com>
2024-10-09 00:17:52 +0530

    repository: interface for ConfessionModel
    Signed-off-by: Dev-raj01 <imdevilrj@gmail.com>

---
+++ src/main/java/com/lamma/SpringWhisper/repository/ConfessionRepo.java
@@ -0,0 +1,9 @@
+package com.lamma.SpringWhisper.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lamma.SpringWhisper.model.ConfessionModel;
+
+public interface ConfessionRepo extends JpaRepository<ConfessionModel, String> {
+
+}
