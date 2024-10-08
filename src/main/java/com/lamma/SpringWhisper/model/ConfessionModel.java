226dacea905fa8cb270ba4a0bfbc31f29352a99c
Dev-raj01 <imdevilrj@gmail.com>
2024-10-09 00:17:44 +0530

    models: add getters and setters for ConfessionModel
    Signed-off-by: Dev-raj01 <imdevilrj@gmail.com>

--- src/main/java/com/lamma/SpringWhisper/model/ConfessionModel.java
+++ src/main/java/com/lamma/SpringWhisper/model/ConfessionModel.java
@@ -30,4 +30,64 @@ public class ConfessionModel {
 
     @Column(name = "updated_at")
     private Time updatedAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Time getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Time createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Time getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Time updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public ConfessionModel(String id, String from, String to, String message, Time createdAt, Time updatedAt) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.message = message;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public ConfessionModel() {
+    }
 }
